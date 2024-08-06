89806d34b6f659f18fd0828a0f9ce52cb4adf8f6
lixiaoxi <1171804323@qq.com>
2024-08-06 14:16:37 +0800

    着色器类

---
+++ 06_Class_GLSL/shader.h
@@ -0,0 +1,27 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include <glad/glad.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class Shader
+{
+public:
+    //读取并构造着色器
+    Shader(const char* vertexPath,const char* fragmentPath);
+    ~Shader();
+    //激活使用程序
+    void use();
+    //uniform工具
+    void setBool(const std::string& name,bool value) const;
+    void setInt(const std::string &name, int value) const;   
+    void setFloat(const std::string &name, float value) const;
+
+private:
+    //程序ID
+    unsigned int ID;
+};
+
+#endif
